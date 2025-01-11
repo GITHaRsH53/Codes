7b56e9e93975189b4d301c5af0f5f73eaacedeb1
Harsh Thakur <guessmenot2003@gmail.com>
2025-01-11 23:20:23 +0530

    Time complexity corrected it's O(sqrt(n) + log(n))

--- Trial_division_method.cpp
+++ Trial_division_method.cpp
@@ -1,4 +1,5 @@
 // trial divison method to find prime factors of a number
+// O(sqrt(n) + log(n)) time complexity
 
 #include<bits/stdc++.h> 
 using namespace std;
@@ -7,15 +8,15 @@ void solve(){
     int n;
     cin >> n;
     vector<int> fact;
-    for(int d=2; d*d<=n; d++){
+    for(int d=2; d*d<=n; d++){  // iterate till sqrt(n)
         if(n%d==0){   // divisible by 2
             fact.push_back(d);
-            while(n%d==0){ // remove all the multiples of 2(similarly for 3,4,etc)
+            while(n%d==0){ // O(log(n)) time complexity  for entire for loop as  // remove all the multiples of 2(similarly for 3,4,etc)
                 n/=d;
             }
         }
     }
-    if(n>1) fact.push_back(n);
+    if(n>1) fact.push_back(n); // at last if n is left as prime number then add it to the list
     for(auto &it:fact)  cout << it << " ";
 
 }
